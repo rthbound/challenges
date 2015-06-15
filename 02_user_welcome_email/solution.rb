17a8ad9e3d765c4e9be62a5ea5829cd0b070c819
Ryan Hosford <tad.hosford@gmail.com>
2015-06-15 16:02:14 -0500

    Ideal controller would utilize strong_parameters

--- 02_user_welcome_email/solution.rb
+++ 02_user_welcome_email/solution.rb
@@ -11,8 +11,13 @@ end
 
 class UsersController < ActionController::Base
   def create
-    @user = User.create!(params[:user])
+    @user = User.create!(user_params)
     UserMailer.welcome_email(@user).deliver_later
     render text: "You're signed up!"
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :email_confirmation, :name, :username)
   end
 end
